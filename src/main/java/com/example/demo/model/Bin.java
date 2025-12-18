615f65d14f74fd1002aee04d08254b8ecda430a4
bala-amypo <bala@amypo.in>
2025-12-18 14:50:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Bin.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+
+@entity
+public class Bin {
+    private long id ;
+    private string idetnifier;
+    private string locationdescription
+}
