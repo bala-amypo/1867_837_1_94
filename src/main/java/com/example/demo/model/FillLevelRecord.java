6315f2d49e3f6139dd1fe42a19e9cd26c677a059
bala-amypo <bala@amypo.in>
2025-12-19 04:27:19 +0000

    User commit

--- src/main/java/com/example/demo/model/FillLevelRecord.java
+++ src/main/java/com/example/demo/model/FillLevelRecord.java
@@ -5,3 +5,56 @@ import java.sql.Timestamp;
 
 @Entity
 public class FillLevelRecord {
+    private Long id;
+    private Bin bin;
+    private Double fillPercentage;
+    private Timestamp recordedAt;
+    private Boolean isWeekend;
+    
+    public FillLevelRecord(
+            Bin bin,
+            Double fillPercentage,
+            Timestamp recordedAt,
+            Boolean isWeekend
+    ) {
+        this.bin = bin;
+        this.fillPercentage = fillPercentage;
+        this.recordedAt = recordedAt;
+        this.isWeekend = isWeekend;
+    }
+  public Long getId() {
+        return id;
+    }
+
+    public Bin getBin() {
+        return bin;
+    }
+
+    public void setBin(Bin bin) {
+        this.bin = bin;
+    }
+
+    public Double getFillPercentage() {
+        return fillPercentage;
+    }
+
+    public void setFillPercentage(Double fillPercentage) {
+        this.fillPercentage = fillPercentage;
+    }
+
+    public Timestamp getRecordedAt() {
+        return recordedAt;
+    }
+
+    public void setRecordedAt(Timestamp recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+
+    public Boolean getIsWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(Boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+}
