80857bf5d003309aa556ed784beb44ba81805a84
bala-amypo <bala@amypo.in>
2025-12-26 15:32:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/FillLevelRecordRepository.java
+++ src/main/java/com/example/demo/repository/FillLevelRecordRepository.java
@@ -4,19 +4,10 @@ import com.example.demo.model.Bin;
 import com.example.demo.model.FillLevelRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-public interface FillLevelRecordRepository extends JpaRepository<FillLevelRecord, Long> {
-
-    List<FillLevelRecord> findByBinOrderByRecordedAtDesc(Bin bin);
+public interface FillLevelRecordRepository
+        extends JpaRepository<FillLevelRecord, Long> {
 
     Optional<FillLevelRecord> findTop1ByBinOrderByRecordedAtDesc(Bin bin);
-
-    List<FillLevelRecord> findByBinAndRecordedAtBetween(
-            Bin bin,
-            LocalDateTime start,
-            LocalDateTime end
-    );
 }
