1fc0442596ae042c151999ff67decab4e1cfce1a
bala-amypo <bala@amypo.in>
2025-12-30 05:05:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -9,9 +9,8 @@ public class AuthResponse {
 
     private String token;
 
-    @Email(message = "Email should be valid")
+  
     private String email;
 
-    @NotBlank(message = "Role is required")
     private String role;
 }
